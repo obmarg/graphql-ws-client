b71e31dd192a50cd66a94f75e491c1ebc042dd9e
Graeme Coupar <graeme@turbofish.tech>
2025-09-28 13:45:22 +0100

    feat: support multiple tungstenite versions

--- src/native.rs
+++ src/native.rs
@@ -1,60 +1,23 @@
-use futures_lite::{Stream, StreamExt};
-use futures_sink::Sink;
-use tungstenite::{self, protocol::CloseFrame};
+#[cfg(feature = "tungstenite-0.23")]
+#[cfg_attr(docsrs, doc(cfg(feature = "tungstenite-0.23")))]
+mod tungstenite_0_23;
 
-use crate::{Error, Message, sink_ext::SinkExt};
+#[cfg(feature = "tungstenite-0.24")]
+#[cfg_attr(docsrs, doc(cfg(feature = "tungstenite-0.24")))]
+mod tungstenite_0_24;
 
-#[cfg_attr(docsrs, doc(cfg(feature = "tungstenite")))]
-impl<T> crate::client::Connection for T
-where
-    T: Stream<Item = Result<tungstenite::Message, tungstenite::Error>>
-        + Sink<tungstenite::Message>
-        + Send
-        + Sync
-        + Unpin,
-    <T as Sink<tungstenite::Message>>::Error: std::fmt::Display,
-{
-    async fn receive(&mut self) -> Option<Message> {
-        loop {
-            match self.next().await? {
-                Ok(tungstenite::Message::Text(text)) => {
-                    return Some(crate::client::Message::Text(text));
-                }
-                Ok(tungstenite::Message::Ping(_)) => return Some(crate::client::Message::Ping),
-                Ok(tungstenite::Message::Pong(_)) => return Some(crate::client::Message::Pong),
-                Ok(tungstenite::Message::Close(frame)) => {
-                    return Some(crate::client::Message::Close {
-                        code: frame.as_ref().map(|frame| frame.code.into()),
-                        reason: frame.map(|frame| frame.reason.to_string()),
-                    });
-                }
-                Ok(tungstenite::Message::Frame(_) | tungstenite::Message::Binary(_)) => continue,
-                Err(error) => {
-                    #[allow(unused)]
-                    let error = error;
-                    crate::logging::warning!("error receiving message: {error:?}");
-                    return None;
-                }
-            }
-        }
-    }
+#[cfg(feature = "tungstenite-0.25")]
+#[cfg_attr(docsrs, doc(cfg(feature = "tungstenite-0.25")))]
+mod tungstenite_0_25;
 
-    async fn send(&mut self, message: crate::client::Message) -> Result<(), Error> {
-        <Self as SinkExt<tungstenite::Message>>::send(
-            self,
-            match message {
-                crate::client::Message::Text(text) => tungstenite::Message::Text(text),
-                crate::client::Message::Close { code, reason } => {
-                    tungstenite::Message::Close(code.zip(reason).map(|(code, reason)| CloseFrame {
-                        code: code.into(),
-                        reason: reason.into(),
-                    }))
-                }
-                crate::client::Message::Ping => tungstenite::Message::Ping(vec![]),
-                crate::client::Message::Pong => tungstenite::Message::Pong(vec![]),
-            },
-        )
-        .await
-        .map_err(|error| Error::Send(error.to_string()))
-    }
-}
+#[cfg(feature = "tungstenite-0.26")]
+#[cfg_attr(docsrs, doc(cfg(feature = "tungstenite-0.26")))]
+mod tungstenite_0_26;
+
+#[cfg(feature = "tungstenite-0.27")]
+#[cfg_attr(docsrs, doc(cfg(feature = "tungstenite-0.27")))]
+mod tungstenite_0_27;
+
+#[cfg(feature = "tungstenite-0.28")]
+#[cfg_attr(docsrs, doc(cfg(feature = "tungstenite-0.28")))]
+mod tungstenite_0_28;
